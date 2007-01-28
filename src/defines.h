390ec1bac213b24578b74177e83e0b46f0fec098
svenor <svenover@svenover.de>
2007-01-28 16:16:40 +0000

    * Christian Marillat reported build problems on alpha, where the __bswap_constant_xx macros seem not to be available. Changed defines in defines.h not to use them anymore.

--- src/defines.h
+++ src/defines.h
@@ -41,9 +41,11 @@ static inline int videostream(int s=0)
   }
 
 #if __BYTE_ORDER == __LITTLE_ENDIAN
-#define mbo32(x) (__bswap_constant_32(x))
+#define mbo32(x) \
+      ((((x) & 0xff000000) >> 24) | (((x) & 0x00ff0000) >>  8) | \
+       (((x) & 0x0000ff00) <<  8) | (((x) & 0x000000ff) << 24))
 #define htom32(x) (__bswap_32(x))
-#define mbo16(x) (__bswap_constant_16(x))
+#define mbo16(x) ((((x) >> 8) & 0xff) | (((x) & 0xff) << 8))
 #define htom16(x) (__bswap_16(x))
 #else
 #define mbo32(x) (x)
