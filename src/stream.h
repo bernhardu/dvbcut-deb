399125fdf8b6e11209d8fff78ab430859bbcc088
too-tired <too-tired@users.sourceforge.net>
2007-01-30 18:51:54 +0000

    fix assertion failures on Windows; update ChangeLog

--- src/stream.h
+++ src/stream.h
@@ -54,6 +54,7 @@ protected:
     avcc->bit_rate=9500000;
     avcc->rc_max_rate=9500000;
     avcc->rc_buffer_size=224*1024*8;
+    avcc->rc_buffer_aggressivity=1.0;
     avcc->rc_initial_buffer_occupancy = avcc->rc_buffer_size*3/4;
     avcc->qmax=2;
     avcc->mb_lmax= FF_QP2LAMBDA * 2;
