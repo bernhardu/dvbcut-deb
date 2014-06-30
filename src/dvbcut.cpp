ac3f3b70990ba3afff4ad475dff168daa7e59ee5
Bernhard Übelacker <bernhardu@vr-web.de>
2014-07-01 01:36:59 +0200

    Mplayer2 uses different indices for audio streams.
    Bug-Debian: https://bugs.debian.org/753369

--- src/dvbcut.cpp
+++ src/dvbcut.cpp
@@ -1182,7 +1182,7 @@ void dvbcut::playPlay()
   arguments << "-geometry" << QString().sprintf("%dx%d+0+0",int(ui->imagedisplay->width()),int(ui->imagedisplay->height()));
 
   if (currentaudiotrack>=0 && currentaudiotrack<mpg->getaudiostreams()) {
-    arguments << "-aid" << QString().sprintf("0x%x",int(mpg->mplayeraudioid(currentaudiotrack)));
+    arguments << "-aid" << QString().sprintf("0x%x",int(currentaudiotrack));
     }
     
   // for now, pass all filenames from the current one up to the last one
