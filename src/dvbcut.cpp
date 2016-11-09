7972e479b2cb7940d680879cf00aefef4555f8f5
Bernhard Übelacker <bernhardu@mailbox.org>
2016-11-09 10:38:35 +0100

    Undo change for mplayer2 in audio track selection when playing.
    This was needed because mplayer was dropped in debian and replaced
    by mplayer2 or mpv (commit ac3f3b70).
    As mplayer was now reintroduced and mplayer2 dropped, we change back
    to the original state.

--- src/dvbcut.cpp
+++ src/dvbcut.cpp
@@ -1232,8 +1232,8 @@ void dvbcut::playPlay()
   arguments << "-geometry" << QString().sprintf("%dx%d+0+0",int(ui->imagedisplay->width()),int(ui->imagedisplay->height()));
 
   if (currentaudiotrack>=0 && currentaudiotrack<mpg->getaudiostreams()) {
-    arguments << "-aid" << QString().sprintf("0x%x",int(currentaudiotrack));
-    }
+    arguments << "-aid" << QString().sprintf("0x%x", int(mpg->mplayeraudioid(currentaudiotrack)));
+  }
     
   // for now, pass all filenames from the current one up to the last one
   std::list<std::string>::const_iterator it = mpgfilen.begin();
