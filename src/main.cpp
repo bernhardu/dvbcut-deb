2c32d44cbf6f53eec2ac741b298df82c00e32ef9
Bernhard Übelacker <bernhardu@mailbox.org>
2016-11-16 16:51:59 +0100

    Remove gcc 6 warning about a required space between literal and string macro.

--- src/main.cpp
+++ src/main.cpp
@@ -52,7 +52,7 @@ static char *argv0;
 void
 usage_exit(int rv=1) {
   fprintf(stderr,
-    "Usage ("VERSION_STRING"):\n"
+    "Usage (" VERSION_STRING "):\n"
     "  %s -generateidx [-idx <idxfilename>] [<mpgfilename> ...]\n"
     "  %s -batch [ OPTIONS ] <prjfilename> | <mpgfilename> ...\n\n"
     "OPTIONS: -cut 4:3|16:9|TS|TS2|<list>, -exp <expfilename>,\n"
